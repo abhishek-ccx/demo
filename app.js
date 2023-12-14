f05e66619f5c67f9702959f5f32341bf499dfb24
Abhishek Kumar <abhishek.kumar@changecx.com>
2023-12-14 14:43:52 +0530

    initial code

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
 
 const userRoutes = require("./routes/userRoutes");
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
 
 app.use("/api/v1/user", userRoutes);
 
